7d1bfdc38f126079ed2a79177ce05b2a997aa8ce
Eyal Amir <eyalamirmusic@gmail.com>
2025-05-31 10:07:10 -0700

    Added CMRC to load shaders

--- src/01-primitive/Shader.h
+++ src/01-primitive/Shader.h
@@ -1,30 +1,11 @@
 #pragma once
 
+#include <cmrc/cmrc.hpp>
+CMRC_DECLARE(primitiveshaders);
+
 inline const char* getShader()
 {
-    return R"(
-        #include <metal_stdlib>
-        using namespace metal;
-
-        struct v2f
-        {
-            float4 position [[position]];
-            half3 color;
-        };
-
-        v2f vertex vertexMain( uint vertexId [[vertex_id]],
-                               device const float3* positions [[buffer(0)]],
-                               device const float3* colors [[buffer(1)]] )
-        {
-            v2f o;
-            o.position = float4( positions[ vertexId ], 1.0 );
-            o.color = half3 ( colors[ vertexId ] );
-            return o;
-        }
-
-        half4 fragment fragmentMain( v2f in [[stage_in]] )
-        {
-            return half4( in.color, 1.0 );
-        }
-    )";
+    auto fs = cmrc::primitiveshaders::get_filesystem();
+    auto shader = fs.open("Shaders/Shaders.metal");
+    return shader.begin();
 }
