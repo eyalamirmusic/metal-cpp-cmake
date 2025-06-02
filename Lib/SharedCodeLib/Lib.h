e6aa12ecbd8c5967aab984234d86ed8a19f885f0
Eyal Amir <eyalamirmusic@gmail.com>
2025-06-02 15:46:36 -0700

    Continued adding buffer abstractions

---
+++ Lib/SharedCodeLib/Lib.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE // Metal uses 0..1 depth
+#define GLM_FORCE_LEFT_HANDED // Optional: Metal is right-handed by default
+#define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES // Good for matching Metal struct layout
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "App.h"
--- src/01-primitive/01-primitive.cpp
+++ src/01-primitive/01-primitive.cpp
@@ -1,25 +1,53 @@
-#include <SharedCodeLib/App.h>
+#include <SharedCodeLib/Lib.h>
 #include "Shader.h"
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE // Metal uses 0..1 depth
-#define GLM_FORCE_LEFT_HANDED // Optional: Metal is right-handed by default
-#define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES // Good for matching Metal struct layout
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
+using Vertices = std::vector<glm::vec3>;
 
-// Generate a rotation matrix (angle in radians)
-glm::mat4 makeZRotation(float angle)
+inline size_t getSize(const Vertices& vertices)
 {
-    return glm::rotate(glm::mat4(1.0f), angle, glm::vec3(0.0f, 0.0f, 1.0f));
+    return sizeof(glm::vec3) * vertices.size();
+}
+
+NS::SharedPtr<MTL::Buffer> createNewBuffer(MTL::Device* device, size_t size)
+{
+    return NS::TransferPtr(device->newBuffer(size, MTL::ResourceStorageModeManaged));
+}
+
+NS::SharedPtr<MTL::Buffer> createBufferFrom(MTL::Device* device,
+                                            const Vertices& vertices)
+{
+    auto size = getSize(vertices);
+    auto buf = createNewBuffer(device, size);
+    memcpy(buf->contents(), vertices.data(), size);
+    buf->didModifyRange(NS::Range::Make(0, buf->length()));
+
+    return buf;
+}
+
+glm::mat4 makeCenteredZRotation(float angle, const glm::vec3& center)
+{
+    glm::mat4 translateToOrigin = glm::translate(glm::mat4(1.0f), -center);
+    glm::mat4 rotate = glm::rotate(glm::mat4(1.0f), angle, glm::vec3(0, 0, 1));
+    glm::mat4 translateBack = glm::translate(glm::mat4(1.0f), center);
+    return translateBack * rotate * translateToOrigin;
+}
+
+glm::vec3 computeTriangleCentroid(const Vertices& positions)
+{
+    auto sum = glm::vec3(0.f);
+
+    for (auto& pos: positions)
+        sum += pos;
+
+    return sum / static_cast<float>(positions.size());
 }
 
 static float getCurrentTimeInRadians()
 {
     static auto startTime = std::chrono::high_resolution_clock::now();
     auto now = std::chrono::high_resolution_clock::now();
-    float seconds = std::chrono::duration<float>(now - startTime).count();
+    auto seconds = std::chrono::duration<float>(now - startTime).count();
+
     return seconds; // 1 rad/sec rotation speed
 }
 
@@ -54,45 +82,25 @@ struct Renderer : Apple::Renderer
 
     void buildBuffers()
     {
-        const size_t NumVertices = 3;
+        auto positions =
+            Vertices {{-0.8f, 0.8f, 0.0f}, {0.0f, -0.8f, 0.0f}, {+0.8f, 0.8f, 0.0f}};
 
-        glm::vec3 positions[NumVertices] = {
-            {-0.8f, 0.8f, 0.0f}, {0.0f, -0.8f, 0.0f}, {+0.8f, 0.8f, 0.0f}};
+        auto colors =
+            Vertices {{1.0, 0.3f, 0.2f}, {0.8f, 1.0, 0.0f}, {0.8f, 0.0f, 1.0}};
 
-        glm::vec3 colors[NumVertices] = {
-            {1.0, 0.3f, 0.2f}, {0.8f, 1.0, 0.0f}, {0.8f, 0.0f, 1.0}};
+        center = computeTriangleCentroid(positions);
 
-        const size_t positionsDataSize = NumVertices * sizeof(glm::vec3);
-        const size_t colorDataSize = NumVertices * sizeof(glm::vec3);
-
-        vertexPositions = NS::TransferPtr(
-            device->newBuffer(positionsDataSize, MTL::ResourceStorageModeManaged));
-        vertexColors = NS::TransferPtr(
-            device->newBuffer(colorDataSize, MTL::ResourceStorageModeManaged));
-
-        memcpy(vertexPositions->contents(), positions, positionsDataSize);
-        memcpy(vertexColors->contents(), colors, colorDataSize);
-
-        vertexPositions->didModifyRange(
-            NS::Range::Make(0, vertexPositions->length()));
-        vertexColors->didModifyRange(NS::Range::Make(0, vertexColors->length()));
-
-        uniformBuffer = NS::TransferPtr(
-            device->newBuffer(sizeof(Uniforms), MTL::ResourceStorageModeManaged));
+        vertexPositions = createBufferFrom(device.get(), positions);
+        vertexColors = createBufferFrom(device.get(), positions);
+        uniformBuffer = createNewBuffer(device.get(), sizeof(Uniforms));
     }
 
     void draw() override
     {
-        float angle = getCurrentTimeInRadians(); // You define how angle is updated
-        float c = std::cos(angle);
-        float s = std::sin(angle);
-
-        // Row-major 4x4 rotation matrix (around Z)
-        glm::mat4x4 rotation = {
-            {c, s, 0.f, 0.f}, {-s, c, 0, 0}, {0, 0, 1, 0}, {0, 0, 0, 1}};
+        auto angle = getCurrentTimeInRadians(); // You define how angle is updated
 
         auto* uniforms = static_cast<Uniforms*>(uniformBuffer->contents());
-        uniforms->modelMatrix = rotation;
+        uniforms->modelMatrix = makeCenteredZRotation(angle, center);
         uniformBuffer->didModifyRange(NS::Range::Make(0, sizeof(Uniforms)));
 
         commandEncoder->setRenderPipelineState(pso.get());
@@ -104,6 +112,8 @@ struct Renderer : Apple::Renderer
                                        NS::UInteger(0),
                                        NS::UInteger(3));
     }
+
+    glm::vec3 center {};
 
     NS::SharedPtr<MTL::RenderPipelineState> pso;
     NS::SharedPtr<MTL::Buffer> vertexPositions;
